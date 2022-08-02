f606215913142d6e1870a7a27e7c4726b2164e9c
Mstislav Kazakov <mkazakov@usetech.ru>
2022-08-02 18:41:01 +0300

    Имя сервиса в labels, а не названии метрики

--- openmetrics_liveness_probe/conf.py
+++ openmetrics_liveness_probe/conf.py
@@ -5,7 +5,7 @@ class Settings(BaseSettings):
     HOST: str = "0.0.0.0"
     PORT: int = 8000
     SERVICE_NAME: str
-    NAME_POSTFIX: str = "liveness_probe_unixtime"
+    METRIC_NAME: str = "liveness_probe_unixtime"
 
     class Config:
         env_prefix = "OPENMETRICS_LIVENESS_PROBE_"
--- openmetrics_liveness_probe/openmetrics_liveness_probe.py
+++ openmetrics_liveness_probe/openmetrics_liveness_probe.py
@@ -4,10 +4,9 @@ import prometheus_client
 
 from .conf import settings
 
-METRIC_NAME = f"{settings.SERVICE_NAME}_{settings.NAME_POSTFIX}"
 
 CONSUMER_LIVENESS_PROBE_UNIXTIME = prometheus_client.Gauge(
-    METRIC_NAME, "Unixtime последней liveness probe"
+    settings.METRIC_NAME, "Unixtime последней liveness probe", ["service"]
 )
 
 
@@ -16,4 +15,4 @@ def start_metrics_server(host=settings.HOST, port=settings.PORT):
 
 
 def liveness_probe():
-    CONSUMER_LIVENESS_PROBE_UNIXTIME.set(time())
+    CONSUMER_LIVENESS_PROBE_UNIXTIME.labels(service=settings.SERVICE_NAME).set(time())
