57f10f81e876c2befe68d22338b0585da032786b
Mstislav Kazakov <mkazakov@usetech.ru>
2022-08-03 19:25:54 +0300

    Максимальная версия prometheus_client не определена

--- openmetrics_liveness_probe/__init__.py
+++ openmetrics_liveness_probe/__init__.py
@@ -1,4 +1,4 @@
 from .openmetrics_liveness_probe import liveness_probe, start_metrics_server, settings  # noqa: F401
 
-__version__ = "0.1.4"
+__version__ = "0.1.5"
 __all__ = ["liveness_probe", "start_metrics_server"]
--- tests/test_openmetrics_liveness_probe.py
+++ tests/test_openmetrics_liveness_probe.py
@@ -2,4 +2,4 @@ from openmetrics_liveness_probe import __version__
 
 
 def test_version():
-    assert __version__ == "0.1.4"
+    assert __version__ == "0.1.5"
