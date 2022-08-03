ba15a41560ca287da7fe5efc04de1222830bfe25
Mstislav Kazakov <mkazakov@usetech.ru>
2022-08-03 19:15:32 +0300

    Добавлена возможность обращаться к settings

--- openmetrics_liveness_probe/__init__.py
+++ openmetrics_liveness_probe/__init__.py
@@ -1,4 +1,4 @@
-from .openmetrics_liveness_probe import liveness_probe, start_metrics_server  # noqa: F401
+from .openmetrics_liveness_probe import liveness_probe, start_metrics_server, settings  # noqa: F401
 
-__version__ = "0.1.3"
+__version__ = "0.1.4"
 __all__ = ["liveness_probe", "start_metrics_server"]
