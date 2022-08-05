f7b54fc9a017187aace00a6a8b2967fa39e882e8
Никитин Даниил Геннадьевич <dnikitin@usetech.ru>
2022-08-05 12:03:56 +0300

    Добавление многопоточного режима.

--- openmetrics_liveness_probe/__init__.py
+++ openmetrics_liveness_probe/__init__.py
@@ -1,4 +1,5 @@
-from .openmetrics_liveness_probe import liveness_probe, start_metrics_server, settings  # noqa: F401
+from .openmetrics_liveness_probe import liveness_probe  # noqa: F401
+from .openmetrics_liveness_probe import settings, start_metrics_server
 
-__version__ = "0.1.5"
+__version__ = "0.1.6"
 __all__ = ["liveness_probe", "start_metrics_server"]
--- openmetrics_liveness_probe/conf.py
+++ openmetrics_liveness_probe/conf.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from pydantic import BaseSettings
 
 
@@ -8,10 +10,16 @@ class Settings(BaseSettings):
     SERVICE_NAME: str = "example"
     METRIC_NAME: str = "liveness_probe_unixtime"
     ENABLE_DEFAULT_PROMETHEUS_METRICS: bool = False
+    PROMETHEUS_MULTIPROC_DIR: Optional[str] = None
 
     class Config:
         env_prefix = "OPENMETRICS_LIVENESS_PROBE_"
         case_sensitive = True
+        fields = {
+            "PROMETHEUS_MULTIPROC_DIR": {
+                "env": "PROMETHEUS_MULTIPROC_DIR",
+            },
+        }
 
 
 settings = Settings()
